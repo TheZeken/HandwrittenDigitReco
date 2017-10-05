1f721aa16861e91a21114a52cf84c58241709732
ishwar <ishwar.purushotham@gmail.com>
2017-10-06 00:14:35 +0200

    freeman more work
    some more work on freeman

--- freeman.py
+++ freeman.py
@@ -16,12 +16,19 @@ size_x = 28
 size_y = 28
 #directions  =   [0,1,2,3,4,5,6,7] 
 directions = 7
+
+
+change_x = [0,1,1]
+
+"""
 change_x    =   [-1,0, 1,
                   1,   1,
                   0,-1,-1]
 change_y    =   [-1,-1,-1,
                  0,  1,
                  1, 1,0]
+"""
+
 
 start_x = 0
 start_y = 0
@@ -106,30 +113,36 @@ curr_y = start_y
 visited[iix,iiy] = 1
 
 """
-find first pixel - in a good image this will be the topmost and leftmost and this will be the starting point
-check the 8 pixels around this pixel in a clockwise direction to determine the possible direction to move
+1. find first pixel - 
+in a good image this will be the topmost and this will be the starting point
+
+2.check the 8 pixels around this pixel in a clockwise direction to determine the possible direction to move
 this next pixel must be at the boundary (ie white space) and must be consecutive
-create a grid of visited pixels
+create a grid of visited pixels so that we don't back track
+
 """"
 
-# this is to test whether the next consecutive pixel is a contour or no
+# this is to test whether the next consecutive pixel is a contour or not
 def iscontour(x,y):
-    contour = False
+    iscontour = False
     for (i, deltax) in enumerate(change_x):
             deltay = change_y[i]
             if pixels[x+deltax,y+deltay] == 0:
-                contour = True
-                break  # as long as there is atleast 1 white pixel, it is good
-    return contour
-        
+                iscontour = True
+                break  # as long as there is at least 1 adjacent white pixel then this pixel is a contour
+    return iscontour
+
+# this is to see if the next pixel to be moved into is feasible
 def feasible (x,y):
         global pixels
-        if pixels[x,y] == 0 and : 
-
-            
+        if pixels[x,y] == 255 and iscontour(x,y): 
+            return true
+        return false
+    
+#move into the first feasible pixel working in a clockwise direction           
 visited = np.zeros (size_x,size_y)
 for dirs in range(0,directions):
-    potential_next_x = 
+    if (feasible(visited+change_x[direction],+change_y[direction]))
 
 
 
