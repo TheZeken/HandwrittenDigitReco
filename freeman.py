2a8713822aa157b3f1b3f3521cf4d2735e23e085
ishwar <ishwar.purushotham@gmail.com>
2017-10-07 23:29:38 +0200

    freemancodeupdate
    needs more work, but closer to completion

--- freeman.py
+++ freeman.py
@@ -8,12 +8,16 @@ Created on Mon Oct  2 08:37:43 2017
 import os
 import struct
 import numpy as np
+import logging as lg
+
+lg.basicConfig(level=lg.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 ##### definitions
 path="C:\\Users\\edmon_000\\Desktop\\mldm year 2\\mldm project\\datasource\\"
 size_x = 28
 size_y = 28
+totalcells = size_x*size_y
 #directions  =   [0,1,2,3,4,5,6,7] 
 directions = 7
 
@@ -84,10 +88,10 @@ def show(image):
 
 
 training_data = list(read(dataset="training", path=path))
-print(len(training_data))
-label,pixels = training_data[1500]
-print(label)
-print(pixels.shape)
+lg.debug(len(training_data))
+label,pixels = training_data[1501]
+lg.debug(label)
+lg.debug(pixels.shape)
 
 for x in range(0,28):
     for y in range (0,28):
@@ -96,24 +100,31 @@ for x in range(0,28):
         else:
             pixels[x,y] = 0
 
-print(pixels[10,12])
+lg.debug(pixels[10,12])
 show(pixels)
 
 
-#look for the top most pixel to start at
-for ii_x in range(0,size_x):
-    for ii_y in range(0,size_y):
-        if pixels[ii_x,ii_y] == 255:
+#find first starting pixel. inner loop is top down, outer loop is left to right
+testpi = np.zeros ((size_x,size_y))
+for ii_x in range(0,size_y):
+    for ii_y in range(0,size_x):
+        if pixels[ii_y,ii_x] == 255:
+            #testpi[ii_y,ii_x] = i-200
+            start_x = ii_x
+            start_y = ii_y
             break
-
-start_x = iix
-start_y = iiy
+    else:
+        continue
+    break
 
 curr_x = start_x
 curr_y = start_y
 
-visited = np.zeros (size_x,size_y)
-visited[iix,iiy] = 1 #we have visited this pixel
+lg.debug(curr_x)
+lg.debug(curr_y)
+
+visited = np.zeros ((size_y,size_x))
+visited[ii_y,ii_x] = 1 #we have visited this pixel
 
 """
 1. find first pixel - 
@@ -124,28 +135,39 @@ this next pixel in question:
    i. must be at the boundary (ie white space) 
    ii. and must be consecutive (connected to the previous pixel)
 3.create a grid of visited pixels so that we don't back track accidentally
-
-""""
+"""
 
 # this is to test whether the next consecutive pixel is a contour or not
-def iscontour(x,y):
+def iscontour(y,x):
     iscontour = False
-    for (i, deltax) in enumerate(change_x):
-            deltay = change_y[i]
-            if pixels[x+deltax,y+deltay] == 0:
+    global change_y,change_x,pixels
+    for (i, delta_x) in enumerate(change_x):
+            delta_y = change_y[i]
+            if pixels[y+delta_y,x+delta_x] == 0:
                 iscontour = True
                 break  # as long as there is at least 1 adjacent white pixel then this pixel is a contour
-    return iscontour
+    return iscontour  
 
 # this is to see if the next pixel to be moved into is feasible
-def feasible (x,y):
-        global pixels
-        if pixels[x,y] == 255 and iscontour(x,y):
-            return true
-        return false
+def feasible (y,x):
+        global pixels,visited
+        if pixels[y,x] == 255 and iscontour(y,x) and visited[y,x] != 1:
+            return True
+        return False
     
-#move into the first feasible pixel working in a clockwise direction           
-for dirs in range(0,directions):
-    if (feasible(curr_x+change_x[direction],+curr_y+change_y[direction])):
-        curr_x = curr_x + change_x[direction]
-        curr_y = curr_y + change_y[direction]
+cellsvisited = 0
+stop = False
+
+while (curr_y != start_y and curr_x != start_x or cellsvisited == 0) and cellsvisited <= 1200:
+    cellsvisited = cellsvisited + 1
+    for dirs in range(0,directions):
+        #move into the first feasible pixel around a current pixel, while working in a clockwise direction
+        if (feasible(curr_y+change_y[dirs],curr_x+change_x[dirs])):
+            visited[curr_y,curr_x] = 1
+            freeman_chain.append(dirs)
+            curr_y = curr_y + change_y[dirs]
+            curr_x = curr_x + change_x[dirs]
+            testpi[curr_y,curr_x] = 30
+            break
+show(testpi)
+  
